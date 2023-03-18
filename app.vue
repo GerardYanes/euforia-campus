bc109460b33931669eb474fa23be96cb9245e1a0
fitodac <fitodac@gmail.com>
2023-03-18 20:30:06 -0300

    app.vue and content files

---
+++ app.vue
@@ -0,0 +1,57 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <main id="mainWrapper">
+		<div class="container max-w-4xl mx-auto">
+			<section id="header">
+				<div class="brand">
+					<h1>Euforia Campus</h1>
+					<h2>Aquest estiu, viu la teva passió</h2>
+				</div>
+			</section>
+
+			<Intro/>
+			<Cta/>
+			<Main/>
+		</div>
+
+		<Footer/>
+  </main>
+</template>
+
+
+<style>
+html{
+	
+}
+
+body{
+	font-family: 'Montserrat', sans-serif;
+	font-size: 20px;
+	@apply bg-indigo-900 text-white font-medium;
+}
+
+#mainWrapper{
+	background-image: url('assets/img/background.webp');
+	background-size: 1100px auto;
+	background-position: center top;
+	background-repeat: repeat-x;
+}
+
+#header{ @apply py-8; }
+
+.brand{
+	background-image: url('assets/img/brand.webp');
+	background-repeat: no-repeat;
+	background-position: center;
+	background-size: auto 100%;
+	font-size: 0;
+	width: 600px;
+	height: 380px;
+
+	@apply text-transparent mx-auto;
+}
+</style>
