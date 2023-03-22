87290ba9b4be8aecfed6bb9fa116108ccc11621b
fitodac <fitodac@gmail.com>
2023-03-22 05:03:19 -0300

    código GTM

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,7 +41,18 @@ export default defineNuxtConfig({
 					href: '/site.webmanifest',
 					rel: 'manifest'
 				}
+			],
 
+			script: [
+				{
+					hid: 'gtmHead',
+					innerHTML: `
+						(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer','G-7JGQB9N1PV');`
+				}
 			]
 		}
 	},
@@ -51,4 +62,5 @@ export default defineNuxtConfig({
 		'@nuxtjs/tailwindcss',
 		'@nuxt/content'
 	]
+
 })
